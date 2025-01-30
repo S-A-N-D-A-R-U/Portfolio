aae40322d4c744a4688fab06b6e7d968bce06833
Naveen S Bandara <sandaru.eng@gmail.com>
2025-01-30 16:10:31 +0530

    project3 jpg to png

--- src/constant/const.ts
+++ src/constant/const.ts
@@ -17,7 +17,7 @@ import Docker from "../assets/docker.png"
 
 import project1 from "../assets/project1.jpg"
 import project2 from "../assets/project2.jpeg"
-import project3 from "../assets/project3.jpg"
+import project3 from "../assets/project3.png"
 
 
 
